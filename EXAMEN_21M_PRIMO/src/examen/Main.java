3107961590329070ffb283a1a94a9bc63f53c66f
Ignacio Gonzalez <ignacio.glezsecadas@gmail.com>
2025-05-21 11:27:47 +0200

    First Commit

---
+++ EXAMEN_21M_PRIMO/src/examen/Main.java
@@ -0,0 +1,12 @@
+package examen;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Buenos días Ceinmark, soy Marta Gomez.");
+		System.out.println("Introduce un numero para saber si es primo o no: ");
+		int num = Integer.parseInt(in.nextLine());
+	}
+}
